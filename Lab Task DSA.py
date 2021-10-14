cdf698258813b927b474e66b6f41478cf523d457
Syed Ali Raza <92259618+sydalirza@users.noreply.github.com>
2021-10-14 21:16:08 +0500

    Add files via upload

---
+++ Lab Task DSA.py
@@ -0,0 +1,59 @@
+
+check = []
+check1 =[]
+from collections import deque
+
+stack = deque()
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+    def push(self,val):
+        self.container.append(val)
+    def pop(self):
+        self.container.pop()
+    def is_balanced(self,val):
+        closed = 0
+        for num in range(0,len(val)):
+            if val[num] == '(':
+                check.append(val[num])
+            elif val[num] == '[':
+                check.append(val[num])
+            elif val[num] == '{':
+                check.append(val[num])  
+            elif val[num] == ')':
+                if len(check) > 0:
+                    if check[len(check)-1] == '(':
+                        check.pop()
+                    else:
+                        check1.append(val[num])
+                else: 
+                    closed = closed + 1
+            elif val[num] == ']':
+                if len(check) > 0:
+                    if check[len(check)-1] == '[':
+                        check.pop()
+                    else:
+                        check1.append(val[num])
+                else: 
+                    closed = closed + 1      
+            elif val[num] == '}':
+                if len(check) > 0:
+                    if check[len(check)-1] == '{':
+                        check.pop()
+                    else:
+                        check1.append(val[num])
+                else: 
+                    closed = closed + 1
+            
+        
+        if len(check) == 0 and closed == 0:
+            print('Balanced Expression')
+        else:
+            print('Unbalanced Expression')      
+
+
+s = Stack()
+s.is_balanced("a+b")
+
+
